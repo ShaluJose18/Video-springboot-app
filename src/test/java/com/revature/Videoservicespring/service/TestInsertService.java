5b6e4a1f4a2e111ab85b4122a4430cc43b0f4423
ShaluJose18 <shalu.jose@revature.com>
2019-12-10 19:08:19 +0530

    Junit testcases committed

--- src/test/java/com/revature/Videoservicespring/service/TestInsertService.java
+++ src/test/java/com/revature/Videoservicespring/service/TestInsertService.java
@@ -1,6 +1,20 @@
 package com.revature.Videoservicespring.service;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.revature.Videoservicespring.dto.VideoDTO;
+import com.revature.Videoservicespring.exception.ServiceException;
+import com.revature.Videoservicespring.model.ReferenceArtifact;
+import com.revature.Videoservicespring.model.ReferenceUrl;
+import com.revature.Videoservicespring.model.SampleProgram;
+import com.revature.Videoservicespring.model.Video;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
@@ -10,8 +24,51 @@ public class TestInsertService {
 	InsertAllVideoService insertion;
 	
 	@Test
-	void addVideo() {
+	void addVideo() throws ServiceException, SQLException {
+		
 		boolean value=false;
+		Video video=new Video();
+		ReferenceArtifact artifact=new ReferenceArtifact();
+		SampleProgram program=new SampleProgram();
+		ReferenceUrl url=new ReferenceUrl();
+		
+		VideoDTO videodto=new VideoDTO();
+		video.setCategory_id(1);
+		video.setDescription("core java");
+		video.setDisplayName("core java");
+		video.setId(8);
+		video.setLevel_id(2);
+		video.setStatus(true);
+		video.setTags("java");
+		video.setTranscript("corejava/56789");
+		video.setVideoName("Java Servlet");
+		videodto.setVideo(video);
+		
+		artifact.setId(5);
+		artifact.setName("java");
+		artifact.setArtifact("java");
+		artifact.setDescription("java");
+		artifact.setVideo_id(8);
+		videodto.setArtifact(artifact);
+		
+		program.setId(5);
+		program.setName("java");
+		program.setArtifact("java");
+		program.setDescription("java");
+		program.setVideo_id(8);
+		videodto.setSampleprogram(program);
+		
+		url.setId(5);
+		url.setName("java");
+		url.setArtifact("java");
+		url.setDescription("java");
+		url.setType("tutorial");
+		url.setVideo_id(8);
+		videodto.setUrl(url);
+		
+		value=insertion.insertVideo(videodto);
+		assertTrue(value);
+		
 	}
 	
 }
